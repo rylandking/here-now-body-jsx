d97a7c25e8ec1df6318141d9b6ce9792b7497ee6
rylandking <ryland@users.noreply.github.com>
2020-04-27 07:47:49 -0600

    Center error message

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -35,37 +35,113 @@ const SignupPage = () => {
   };
 
   return (
-    <>
-      <Head>
-        <title>Sign up</title>
-      </Head>
-      <div>
-        <h2>Sign up</h2>
-        <form onSubmit={handleSubmit}>
-          {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
-          <label htmlFor="name">
-            <input id="name" name="name" type="text" placeholder="Your name" />
-          </label>
-          <label htmlFor="email">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Email address"
-            />
-          </label>
-          <label htmlFor="password">
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Create a password"
-            />
-          </label>
-          <button type="submit">Sign up</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full">
+        <div>
+          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            Create your account
+          </h2>
+        </div>
+        <form className="mt-8" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm">
+            <div>
+              <input
+                htmlFor="name"
+                id="name"
+                aria-label="Your name"
+                name="name"
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Your name"
+              />
+            </div>
+            <div>
+              <input
+                htmlFor="email"
+                id="email"
+                aria-label="Email address"
+                name="email"
+                type="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="-mt-px">
+              <input
+                htmlFor="password"
+                id="password"
+                aria-label="Password"
+                name="password"
+                type="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Create a password"
+              />
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-pink-600 hover:bg-pink-500 focus:outline-none focus:border-pink-700 focus:shadow-outline-pink active:bg-pink-700 transition duration-150 ease-in-out"
+            >
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <svg
+                  className="h-5 w-5 text-pink-500 group-hover:text-pink-400 transition ease-in-out duration-150"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign in
+            </button>
+            <div className="flex justify-center mt-6">
+              {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
+            </div>
+          </div>
         </form>
       </div>
-    </>
+    </div>
+
+    // <>
+    //   <Head>
+    //     <title>Sign up</title>
+    //   </Head>
+    //   <div>
+    //     <h2>Sign up</h2>
+    //     <form onSubmit={handleSubmit}>
+    //       {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
+    //       <label htmlFor="name">
+    //         <input id="name" name="name" type="text" placeholder="Your name" />
+    //       </label>
+    //       <label htmlFor="email">
+    //         <input
+    //           id="email"
+    //           name="email"
+    //           type="email"
+    //           placeholder="Email address"
+    //         />
+    //       </label>
+    //       <label htmlFor="password">
+    //         <input
+    //           id="password"
+    //           name="password"
+    //           type="password"
+    //           placeholder="Create a password"
+    //         />
+    //       </label>
+    //       <button type="submit">Sign up</button>
+    //     </form>
+    //   </div>
+    // </>
   );
 };
 
