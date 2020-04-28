df17d8d5ec1ded3a9b23c037c5343fb58c6e8bca
rylandking <ryland@users.noreply.github.com>
2020-04-28 07:51:08 -0600

    Route to / after logout

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,9 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import Router from 'next/router';
 import Header from '../components/Header';
 import { useUser } from '../lib/hooks';
 
 const Admin = () => {
-  const [, { mutate }] = useUser();
+  // call whenever user changes (ex. right after signing up successfully)
+  useEffect(() => {
+    // redirect to home if user is authenticated
+    if (user === null) Router.replace('/');
+  });
+
+  const [user, { mutate }] = useUser();
   const handleLogout = async () => {
     await fetch('/api/auth', {
       method: 'DELETE',
@@ -15,6 +22,7 @@ const Admin = () => {
   return (
     <Fragment>
       <Header />
+      <div>Hello, {user ? user.name : 'stranger'}!</div>
       <button onClick={handleLogout}>Logout</button>
     </Fragment>
   );
